53cab56771e9d62b47a7cfb9709b0aa2d9223a1b
Sergey Trofimov <sergey@trofimov.name>
2023-03-07 17:26:12 +0100

    (chore) Change license to MIT

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 Release date `UNRELEASED`
 
+- (chore) Change license to MIT.
+
 ## `1.1.0-8`
 
 Release date `2023-02-24`
--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject com.github.strojure/memoize-one "1.1.1-9-SNAPSHOT"
   :description " Clojure library for memoization of the single value."
   :url "https://github.com/strojure/memoize-one"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]]
+  :dependencies []
 
-  :profiles {:dev {:dependencies []}})
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.11.1"]]}
+             :dev {:dependencies []}})
