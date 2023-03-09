4b3ee89f612fc5089f0f72d266567685bfaece30
Sergey Trofimov <sergey@trofimov.name>
2023-03-09 15:43:51 +0100

    (chore): Change license to Unlicense

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject com.github.strojure/memoize-one "1.1.2-12-SNAPSHOT"
   :description " Clojure library for memoization of the single value."
   :url "https://github.com/strojure/memoize-one"
-  :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
+  :license {:name "The Unlicense" :url "https://unlicense.org"}
 
   :dependencies []
 
